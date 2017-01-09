e328e6fe87fd93e39b02f65ab455677173c38166
Andrei <andrei.coman.88@gmail.com>
2017-01-09 23:52:50 +0200

    update title real time

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -11,13 +11,14 @@ export default class Layout extends React.Component {
         };
     }
 
+    changeTitle(title) {
+        this.setState({title});
+    }
+
     render() {
-        setTimeout(() => {
-            this.setState({title: "Welcome Will"});
-        }, 2000);
         return (
             <div>
-                <Header title={this.state.title}/>
+                <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
                 <Footer/>
             </div>
         );
