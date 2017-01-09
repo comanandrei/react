97d7849be2889e73784dd7048d3e79f805a6fa52
Andrei Coman <andrei.coman@iquestgroup.com>
2017-01-09 17:34:10 +0200

    Sample

---
+++ src/js/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./header/Title";
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <Title />
+            </div>
+        );
+    }
+}
