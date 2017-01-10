46e2509a258aae373920d26c6af95e78ad2e3432
Andrei <andrei.coman.88@gmail.com>
2017-01-10 02:42:53 +0200

    Routes and query

---
+++ src/js/components/layout/Footer.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Andrei on 10-Jan-17.
+ */
+import React from "react";
+
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; KillerNews.net</p>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
---
+++ src/js/pages/Settings.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Andrei on 10-Jan-17.
+ */
+import React from "react";
+
+export default class Settings extends React.Component {
+    render() {
+        console.log("settings");
+        return (
+            <h1>Settings</h1>
+        );
+    }
+}
